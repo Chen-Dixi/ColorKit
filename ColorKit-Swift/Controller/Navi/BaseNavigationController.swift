75fdc2120635c07fa828cabcc0478b129678bce8
Chen-Dixi <cdxcqu2014@163.com>
2018-05-05 12:28:08 +0800

    first vc

---
+++ ColorKit-Swift/Controller/Navi/BaseNavigationController.swift
@@ -0,0 +1,83 @@
+//
+//  BaseNavigationController.swift
+//  ColorKit-Swift
+//
+//  Created by Dixi-Chen on 2018/5/5.
+//  Copyright © 2018年 Dixi-Chen. All rights reserved.
+//
+
+import UIKit
+
+class BaseNavigationController: UINavigationController, UIGestureRecognizerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        interactivePopGestureRecognizer?.delegate = self
+        setNeedsStatusBarAppearanceUpdate()
+        delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        if animated{
+            interactivePopGestureRecognizer?.isEnabled = false
+        }
+        
+        super.pushViewController(viewController, animated: animated)
+    }
+    
+    override func popToRootViewController(animated: Bool) -> [UIViewController]? {
+        if animated {
+            interactivePopGestureRecognizer?.isEnabled = false
+        }
+        
+        return super.popToRootViewController(animated: animated)
+    }
+    
+    override func popToViewController(_ viewController: UIViewController, animated: Bool) -> [UIViewController]? {
+        if animated {
+            interactivePopGestureRecognizer?.isEnabled = false
+        }
+        
+        return super.popToViewController(viewController, animated: false)
+    }
+    
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if gestureRecognizer == interactivePopGestureRecognizer {
+            if self.viewControllers.count < 2 || self.visibleViewController == self.viewControllers[0] {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    
+    
+    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        interactivePopGestureRecognizer?.isEnabled = true
+    }
+    
+    func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+        let backItem:UIBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: self, action: nil)
+        viewController.navigationItem.backBarButtonItem = backItem
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ColorKit-Swift/ViewController.swift
+++ ColorKit-Swift/ViewController.swift
@@ -7,19 +7,25 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import CoreData
+class ViewController: BaseViewController {
+    
+    
+    @IBOutlet weak var newColorButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    
 
 }
 
